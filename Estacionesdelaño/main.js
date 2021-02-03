6c41ed087f996662f4be2d499324e28773187adc
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-02-03 00:28:39 -0600

    Archivo estaciones del año Unidad 2

---
+++ Estacionesdelaño/main.js
@@ -0,0 +1,22 @@
+var mes=10;
+
+mes=prompt("Agrega un numero del 1 al 12 para identificar una estación del año")
+
+if(mes==12|| mes==1|| mes==2){
+
+  alert("Es Invierno")
+}
+else if(mes==3|| mes==4|| mes==5){
+    alert("Es Primavera")
+}
+else if(mes==6|| mes==7|| mes==8){
+    alert("Es Verano")
+}
+else if(mes==9|| mes==10|| mes==11){
+    alert("Es otoño")
+}
+else{
+  alert("El numero que ingreso no es valido")
+}
+
+alert("No.Carnet: 21003678     Erik Estuardo Hernández López")
